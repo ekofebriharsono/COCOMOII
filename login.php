6b0eeb9a6ceb0e03508225133c051ac652852084
maseko <ekoaewes2006@gmail.com>
2019-12-22 20:42:31 +0700

    add information login failed or logout in form login

--- login.php
+++ login.php
@@ -6,12 +6,10 @@
 
 	<link rel="shortcut icon" href="image/logo_stikom_warna.png" type="image/png">
 
-
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<!------ Include the above in your HEAD tag ---------->
-
 
 	<!--Bootsrap 4 CDN-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
@@ -23,15 +21,36 @@
 
 	<!--Custom styles-->
 	<link rel="stylesheet" type="text/css" href="style/css/loginstyle.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
+
 	<div class="container">
+
 		<div class="d-flex justify-content-center h-100">
 			<div class="card">
 				<div class="card-header">
 					<h3>Sign In</h3>
 					<h6>COCOMO II</h6>
+					<?php if(isset($_GET['pesan'])){
+						$pesan = $_GET['pesan'];
+						if($pesan == "gagal"){ ?>
+					<center>
+						<h6>Username or password incorrect</h6>
+					</center>
+					<?php	} if ($pesan == "logout"){ ?>
+					<center>
+						<h6>Logout Berhasil</h6>
+					</center>
+					<?php	}
+
+					}
+					?>
 					<div class="d-flex justify-content-end social_icon">
 						<span><i class="fab fa-facebook-square"></i></span>
 						<span><i class="fab fa-google-plus-square"></i></span>
@@ -44,14 +63,14 @@
 							<div class="input-group-prepend">
 								<span class="input-group-text"><i class="fas fa-user"></i></span>
 							</div>
-							<input type="text" class="form-control" placeholder="username" name="username">
+							<input type="text" class="form-control" placeholder="username" name="username" required>
 
 						</div>
 						<div class="input-group form-group">
 							<div class="input-group-prepend">
 								<span class="input-group-text"><i class="fas fa-key"></i></span>
 							</div>
-							<input type="password" class="form-control" placeholder="password" name="password">
+							<input type="password" class="form-control" placeholder="password" name="password" required>
 						</div>
 						<div class="row align-items-center remember">
 							<input type="checkbox">Remember Me
