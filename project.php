b69a76909eb59ee7533c716054eb36fa2d8f9827
maseko <ekoaewes2006@gmail.com>
2019-12-28 22:02:30 +0700

    fix layout project

--- project.php
+++ project.php
@@ -3,16 +3,17 @@ session_start();
 include 'proses_php/koneksi.php';
 if(isset($_SESSION['username'])==""){
     echo "<script>window.open('login.php?pesan=belum_login','_self')</script>";
-    } else { 
-?>
+    } else { ?>
 <!DOCTYPE html>
 <html>
-
 <link rel="shortcut icon" href="image/logo_stikom_warna.png" type="image/png">
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 <link href="style/css/style.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+  integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <head>
   <meta name="Author" content="Eko Febri Harsono">
@@ -25,76 +26,108 @@ if(isset($_SESSION['username'])==""){
 
   <?php include('navbar.php'); ?>
 
-  <div id="regForm">
-    <h1>PROJECT COCOMO II</h1>
-    <div class="tab">
-      <hr>
-    </div>
-      <center>
-    <table border="1">
-      <tr>
-        <td>
-          <center>No.</center>
-        </td>
-        <td>
-          <center>Nama Project</center>
-        </td>
-        <td>
-          <center>Total</center>
-        </td>
-        <td colspan="2">
-          <center>Aksi</center>
-        </td>
-      </tr>
-      <?php 
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-4">
+        <div class="box-content">
+          <div class="row">
+            <div class="col-12">
+              <div class="box-content" style="height:120px;">
+                <h1 class="text-center "></h1>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <form id="regInfo" method="GET" action="https://en.wikipedia.org/wiki/COCOMO">
+                <h1>Info</h1>
+                <div class="tab1">
+                </div>
+                <div style="overflow:auto;">
+                  <div style="float:right;">
+                    <p class="text-justify">Project adalah sebuah fitur history atau project yang telah didaftarkan yang
+                      sudah dihitung atupun belum dihitung akan terseimpan kedalam sistem</p>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-8">
+        <div class="box-content">
+          <div id="regForm">
+            <h1>PROJECT COCOMO II</h1>
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <td>
+                    <center><b>No.</b></center>
+                  </td>
+                  <td>
+                    <center><b>Project Name</b></center>
+                  </td>
+                  <td>
+                    <center><b>Total</b></center>
+                  </td>
+                  <td colspan="2">
+                    <center><b>Action</b></center>
+                  </td>
+                </tr>
+              </thead>
+              <?php 
           $id_user =  $_SESSION['id_user'];
           $sql = "select * from project where id_user='$id_user'";
           $data = mysqli_query($con,$sql);
           $no = 0;
           while($d = mysqli_fetch_array($data)){
-            $no++;
-        ?>
-      <tr>
-        <td>
-          <center><?php echo $no; ?></center>
-        </td>
-        <td><?php echo $d['nama_project']; ?></td>
-        <td>
-          <center><?php echo $d['total']; ?></center>
-        </td>
-        <td>
-          <form action="tambah_project.php" method="get">
-            <input hidden type="text" name="project" value="<?php echo $d['id_project']; ?>">
-            <button type="submit">Edit</button>
-          </form>
-        </td>
-        <td>
-          <form action="proses_php/prosesTambahProject.php" method="get">
-            <input hidden type="text" name="idProject" value="<?php echo $d['id_project']; ?>">
-            <button type="submit" name="submitDeleteProject" style="background-color: #df2929;">Delete</button>
-          </form>
-        </td>
-      </tr>
-      <?php } ?>
-    </table>
-    </center>
-
-    <div style="overflow:auto;">
-      <div style="float:right;">
-        <form action="tambah_project.php">
-          <button type="submit">Tambah Project</button>
-        </form>
+            $no++; ?>
+              <tr>
+                <td>
+                  <center><?php echo $no; ?></center>
+                </td>
+                <td><?php echo $d['nama_project']; ?></td>
+                <td>
+                  <center><?php echo $d['total']; ?></center>
+                </td>
+                <td>
+                  <form action="tambah_project.php" method="get">
+                    <input hidden type="text" name="project" value="<?php echo $d['id_project']; ?>">
+                    <button type="submit">Edit</button>
+                  </form>
+                </td>
+                <td>
+                  <form action="proses_php/prosesTambahProject.php" method="get">
+                    <input hidden type="text" name="idProject" value="<?php echo $d['id_project']; ?>">
+                    <button type="submit" name="submitDeleteProject" style="background-color: #df2929;">Delete</button>
+                  </form>
+                </td>
+              </tr>
+              <?php } ?>
+            </table>
+            <div style="overflow:auto;">
+              <div style="float:right;">
+                <form action="tambah_project.php">
+                  <button type="submit">Tambah Project</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
-    <div style="text-align:center;margin-top:40px;">
-      <p>&copy; 2019 LUG</p>
-    </div>
-  </div>
 
 </body>
 
 <script src="js/formPagination.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 
 </html>
 <?php 
